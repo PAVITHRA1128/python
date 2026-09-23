132a50f85396a54f9de8a190b1c3e8efbeca03cb
Pavithra.G <pavithragovindasamy54@gmail.com>
2026-09-23 09:01:16 +0530

    uploaded
    portfolio

---
+++ serpent syntax.py
@@ -0,0 +1,38 @@
+WELCOME TO GUESS THE NUMBER
+DO YOU WANT TO PLAY:YES / NOyes
+LET'S PLAY
+GUESS MY NUMBER BETWEEN 1 TO 100
+
+Enter Your Guess50
+
+HIGH
+Try something Lower
+
+Enter Your Guess30
+
+HIGH
+Try something Lower
+
+Enter Your Guess20
+
+LOW
+Try something Higher
+
+Enter Your Guess25
+
+LOW
+Try something Higher
+
+Enter Your Guess28
+
+HIGH
+Try something Lower
+
+Enter Your Guess26
+
+CONGRATULATIONS! YOU GUESSED THE CORRECT NUMBER!
+You have Guessed The number in 6 Tries
+
+WELCOME TO GUESS THE NUMBER
+DO YOU WANT TO PLAY:YES / NOno
+BYE BYE COME PLAY LATER!
